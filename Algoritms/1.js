ad75922f49a073e7be9f7ef85d2859b5a4c1c321
Greyewi <greyewi@gmail.com>
2022-05-22 16:59:34 +0500

    add JS

---
+++ Algoritms/1.js
@@ -0,0 +1,46 @@
+// basic data types
+{
+
+  const n = 55 // type of number
+  let y = 200
+
+  let s = "sss" // type of string
+  let r = 'dddd'
+
+  let bb = true // type of boolean
+  let b = false
+
+  let u // type of undefined
+  let uu = undefined // type of undefined
+
+  let nu = null // type of null (object)
+
+  let na = NaN // type of NaN (number)
+
+// console.log(y)
+// y = 400
+// console.log(y)
+
+// operators
+
+  y = 400 // assigment
+  y = y - 100 // subtraction + assigment
+  y = y + 100 // subtraction + addition
+  y = y * 100 // subtraction + multiply
+  y = y / 100 // subtraction + division
+
+  y = y + '100' // subtraction + concatenation
+
+//console.log(y)
+  y = y * true // subtraction + multiply
+//console.log(y)
+
+}
+// https://gist.github.com/Greyewi/3c36123831ba7eb170598ba1131a316f
+
+{ // Begin1
+  const a = 10
+  const P = 4 * a
+
+  console.log(P)
+}
